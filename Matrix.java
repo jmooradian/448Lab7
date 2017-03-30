9e5c4b524f7f326f709387151cf2686bed44727b
jmooradian <juniorbirdman52@gmail.com>
2017-03-30 18:02:52 -0500

    Added determinant function.

--- Matrix.java
+++ Matrix.java
@@ -3,7 +3,26 @@ import java.math.*;
 
 public class Matrix
 {
-	
+	public static double determinant(int size, double[][] matrix)
+	{
+		double det = 0;
+		if(size == 1)
+		{
+			det = matrix[0][0];
+		}
+		else if(size == 2)
+		{
+			det = matrix[0][0]*matrix[1][1] - matrix[1][0]*matrix[0][1];
+		}
+		else
+		{
+			for (int i = 0; i < size; ++i)
+			{
+				det += Math.pow(-1.0, (double)i) * matrix[0][i] * determinant(size-1,subMatrix(0, i, matrix, size));
+			}
+        }
+        return det;
+	}
 	
 	public static double[][] subMatrix(int r, int c, double[][] matrix, int size)
 	{
